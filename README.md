8d851a936781ecee8f958524bc3b03db50d1c4a3
hapjs <mailhap@qq.com>
2016-12-13 22:34:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# rollup-babel-boilerplate
+
+这是一个脚手架项目，
+
+通过 rollup 和 babel，你可以充分利用 es6 的强大特性和 npm 的丰富资源，快速地开发出自己组件或者库。
+
+## 开发环境
+
+1. 下载源代码，`git clone https://github.com/hapjs/rollup-babel-boilerplate.git`，或[点击下载](https://github.com/hapjs/rollup-babel-boilerplate/archive/master.zip)
+2. 进入目录，`cd rollup-babel-boilerplate`
+3. 安装依赖库，`npm i`
+4. 编译代码，`npm run build`
+
+## 问题反馈
+
+mailhap@qq.com
+
+## 参考资料
+
+http://rollupjs.org/guide/
+
+https://github.com/rollup/rollup-plugin-babel
---
+++ build.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var rollup = require('rollup').rollup;
+var babel = require('rollup-plugin-babel');
+var commonjs = require('rollup-plugin-commonjs');
+
+rollup({
+  entry: 'src/index.js',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    })
+  ]
+}).then(function(bundle){
+    return write('build/index.js', bundle.generate({
+        format: 'umd',
+        banner: ''
+    }).code);
+});
+
+function write(dest, code){
+    return new Promise(function(resolve, reject){
+        fs.writeFile(dest, code, function(err){
+            if(err) return reject(err);
+            resolve();
+        });
+    });
+}
